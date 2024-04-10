c02dd3b4c9c2fa2abbc8e6a5cf239f2d113120c7
Sasank <sasank.desaraju@ufl.edu>
2024-04-10 16:42:08 -0400

    removed comments from the interactive.sh function call

--- interactive.sh
+++ interactive.sh
@@ -1,12 +1,13 @@
 srun \
   --ntasks=1 \
   --cpus-per-task=1 \
-  # This is the account name of the PI or the lab
   --account=aiinmedicine \
-  # This is the corresponding QOS name; usually same as account name
   --qos=aiinmedicine \
   --partition=gpu \
   --gpus=a100:1 \
   --mem=8gb \
   --time=08:00:00 \
   --pty bash -i
+
+# The account is the account name of the PI or the lab
+# The qos is the corresponding QOS (Quality of Service) name; usually same as account name
